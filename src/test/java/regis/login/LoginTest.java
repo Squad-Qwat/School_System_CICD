18cb0eea3e7453a0cbbd9b37cc74515b0286c742
Afriza Gilleon Ginting <41586762+afrizagilleon@users.noreply.github.com>
2026-06-27 21:18:02 +0700

    ct: test success

--- src/test/java/regis/login/LoginTest.java
+++ src/test/java/regis/login/LoginTest.java
@@ -8,7 +8,7 @@ class LoginTest {
     void testLogin() {
         Login l = new Login("Budi", "password123");
 
-        assertTrue(l.login("Budi", "password12300"));
+        assertTrue(l.login("Budi", "password123"));
         assertFalse(l.login("Budi", "wrong"));
         assertFalse(l.login("Andi", "password123"));
     }
